6e489bc97b50d83312678043b7795fff54a6146d
김기섭 <kiseop91@naver.com>
2019-07-28 11:35:19 +0900

    모든 기능추가 버그 수정완료

--- main.go
+++ main.go
@@ -13,21 +13,21 @@ type GimulType int
 const (
 	GimulTypeNone = -1 + iota
 	GimulTypeGreenWang
-	GimulTypeGreenJa
-	GimulTypeGreenJang
-	GimulTypeGreenSang
+	GimulTypeGreenJa   //1
+	GimulTypeGreenJang //2
+	GimulTypeGreenSang //3
 	GimulTypeGreenQueen
 	GimulTypeRedWang
-	GimulTypeRedJa
-	GimulTypeRedJang
-	GimulTypeRedSang
+	GimulTypeRedJa   //6
+	GimulTypeRedJang //7
+	GimulTypeRedSang //8
 	GimulTypeRedQueen
 	GimulTypeMax
 )
 
 const (
 	ScreenWidth  = 480
-	ScreenHeight = 412 //362
+	ScreenHeight = 462 //362
 	S            = 20
 	GridWidth    = 116
 	T            = 23
@@ -46,9 +46,11 @@ const (
 )
 
 var (
+	deadboard      [6]GimulType
 	board          [BoardWidth][BoardHeight]GimulType
 	bgimg          *ebiten.Image
 	gimulImgs      [GimulTypeMax]*ebiten.Image
+	mgimulImgs     [GimulTypeMax - 1]*ebiten.Image
 	selected       bool
 	selectedX      int
 	selectedY      int
@@ -57,7 +59,28 @@ var (
 	gameover       bool
 	GreenJaToQueen bool
 	RedJaToQueen   bool
+	idx            int
+	cnt            int
+	deadsel        bool
+	deadtar        GimulType
 )
+
+func initiate() {
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 3; j++ {
+			board[i][j] = GimulTypeNone
+		}
+	}
+	board[0][0] = GimulTypeGreenSang
+	board[0][1] = GimulTypeGreenWang
+	board[0][2] = GimulTypeGreenJang
+	board[1][1] = GimulTypeGreenJa
+
+	board[3][0] = GimulTypeRedSang
+	board[3][1] = GimulTypeRedWang
+	board[3][2] = GimulTypeRedJang
+	board[2][1] = GimulTypeRedJa
+}
 
 func GetTeamType(gimulType GimulType) TeamType {
 	if gimulType == GimulTypeGreenJa ||
@@ -88,12 +111,22 @@ func OnDie(gimulType GimulType) {
 	if gimulType == GimulTypeGreenWang ||
 		gimulType == GimulTypeRedWang {
 		gameover = true
+		//initiate()
 	}
 }
 
 func move(prevX, prevY, tarX, tarY int) {
 	if isMoveble(prevX, prevY, tarX, tarY) {
 		OnDie(board[tarX][tarY])
+		if board[tarX][tarY] != GimulTypeNone {
+			if board[tarX][tarY] > 4 {
+				deadboard[cnt] = board[tarX][tarY] - 5
+			} else {
+				deadboard[cnt] = board[tarX][tarY] + 5
+			}
+			cnt += 1
+			cnt %= 6
+		}
 		board[prevX][prevY], board[tarX][tarY] = GimulTypeNone, board[prevX][prevY]
 		selected = false
 		if currentTeam == TeamGreen {
@@ -148,7 +181,49 @@ func update(screen *ebiten.Image) error {
 	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
 		x, y := ebiten.CursorPosition()
 		i, j := x/GridWidth, y/GridHeight
-		if i >= 0 && i < GridWidth && j >= 0 && j < GridHeight {
+		if y > 400 {
+			if x > 0 && x < 40 {
+				idx = 0
+				deadtar = deadboard[idx]
+				deadsel = true
+			}
+			if x > 75 && x < 90 {
+				idx = 1
+				deadtar = deadboard[idx]
+				deadsel = true
+			}
+			if x > 125 && x < 140 {
+				idx = 2
+				deadtar = deadboard[idx]
+				deadsel = true
+			}
+			//
+			if x > 330 && x < 350 {
+				idx = 3
+				deadtar = deadboard[idx]
+				deadsel = true
+			}
+			if x > 385 && x < 395 {
+				idx = 4
+				deadtar = deadboard[idx]
+				deadsel = true
+			}
+			if x > 435 && x < 450 {
+				idx = 5
+				deadtar = deadboard[idx]
+				deadsel = true
+			}
+		} else if i >= 0 && i < GridWidth && j >= 0 && j < GridHeight {
+			if deadsel {
+				if GetTeamType(deadtar) == TeamGreen {
+					currentTeam = TeamRed
+				} else {
+					currentTeam = TeamGreen
+				}
+				board[i][j] = deadtar
+				deadsel = false
+				deadboard[idx] = GimulTypeNone
+			}
 			if selected {
 				if i == selectedX && j == selectedY {
 					selected = false
@@ -197,6 +272,37 @@ func update(screen *ebiten.Image) error {
 		opts := &ebiten.DrawImageOptions{}
 		opts.GeoM.Translate(float64(S+GridWidth*selectedX), float64(T+GridHeight*selectedY))
 		screen.DrawImage(selectedImg, opts)
+	}
+
+	for i := 0; i < 6; i++ {
+		opts := &ebiten.DrawImageOptions{}
+		trim := 0
+		if i >= 3 {
+			trim = 160
+		}
+		if deadboard[i] == GimulTypeGreenQueen {
+			deadboard[i] = GimulTypeGreenJa
+		}
+		if deadboard[i] == GimulTypeRedQueen {
+			deadboard[i] = GimulTypeRedJa
+		}
+		opts.GeoM.Translate(float64(10+50.0*i+trim), 400)
+		switch deadboard[i] {
+		case GimulTypeGreenJa:
+			screen.DrawImage(mgimulImgs[GimulTypeGreenJa], opts)
+		case GimulTypeGreenSang:
+			screen.DrawImage(mgimulImgs[GimulTypeGreenSang], opts)
+		case GimulTypeGreenJang:
+			screen.DrawImage(mgimulImgs[GimulTypeGreenJang], opts)
+			//
+		case GimulTypeRedJa:
+			screen.DrawImage(mgimulImgs[GimulTypeRedJa], opts)
+		case GimulTypeRedSang:
+			screen.DrawImage(mgimulImgs[GimulTypeRedSang], opts)
+		case GimulTypeRedJang:
+			screen.DrawImage(mgimulImgs[GimulTypeRedJang], opts)
+
+		}
 	}
 	return nil
 }
@@ -261,6 +367,44 @@ func main() {
 	selectedImg, _, err = ebitenutil.NewImageFromFile("./selected.PNG", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatalf("read file error : %v ", err)
+	}
+
+	///
+	mgimulImgs[GimulTypeGreenJa], _, err = ebitenutil.NewImageFromFile("./m/mgreen_ja.PNG", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatalf("read file error : %v ", err)
+	}
+
+	mgimulImgs[GimulTypeGreenJang], _, err = ebitenutil.NewImageFromFile("./m/mgreen_jang.PNG", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatalf("read file error : %v ", err)
+	}
+
+	mgimulImgs[GimulTypeGreenSang], _, err = ebitenutil.NewImageFromFile("./m/mgreen_sang.PNG", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatalf("read file error : %v ", err)
+	}
+
+	//
+
+	mgimulImgs[GimulTypeRedJa], _, err = ebitenutil.NewImageFromFile("./m/mred_ja.PNG", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatalf("read file error : %v ", err)
+	}
+
+	mgimulImgs[GimulTypeRedJang], _, err = ebitenutil.NewImageFromFile("./m/mred_jang.PNG", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatalf("read file error : %v ", err)
+	}
+
+	mgimulImgs[GimulTypeRedSang], _, err = ebitenutil.NewImageFromFile("./m/mred_sang.PNG", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatalf("read file error : %v ", err)
+	}
+
+	//	initailize deadboard
+	for i := 0; i < 6; i++ {
+		deadboard[i] = GimulTypeNone
 	}
 
 	//intialize board
